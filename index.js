cead7449afbe5a588ed9956d01098e0c07986216
ahmetyanik <ahmetyank4242@gmail.com>
2022-02-03 21:15:26 +0100

    .env added

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const userRouter = require("./router/user_router");
 const mainRouter = require("./router/main_router");
 const fehlerRouter = require("./router/fehler_router");
 
+const PORT = process.env.PORT || 4025;
+
 const app = express();
 
 
@@ -13,6 +15,6 @@ app.use(fehlerRouter);
 
 
 
-app.listen(3001,()=>{
+app.listen(PORT,()=>{
     console.log("Server arbeitet...");
 })
